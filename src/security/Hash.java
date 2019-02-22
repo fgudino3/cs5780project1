81bb92a4938fef1f2d4c135312a14ac0507318ae
fgudino3 <frankyjg96@aol.com>
2019-02-22 11:20:35 -0800

    fixed the javadoc comments

--- src/security/Hash.java
+++ src/security/Hash.java
@@ -8,17 +8,13 @@ public class Hash {
 	private int ncb;
 	private byte p;
 	private int k;
-
-	/*
+	
+	/**
 	 * Hash object constructor
-	 * 
-	 * @param ndb - number of data bytes
-	 * 
-	 * @param ncb - number of checksum bytes
-	 * 
-	 * @param p - hash function byte pattern
-	 * 
-	 * @param k - hash function k
+	 * @param ndb number of data bytes
+	 * @param ncb number of checksum bytes
+	 * @param p hash function byte pattern
+	 * @param k hash function k
 	 */
 	public Hash(int ndb, int ncb, byte p, int k) {
 		this.ndb = ndb;
@@ -26,39 +22,36 @@ public class Hash {
 		this.p = p;
 		this.k = k;
 	}
-
-	/*
+	
+	/**
 	 * @return the number of bytes a packet occupies
 	 */
 	public int getPacketSize() {
 		return 1 + ndb + ncb;
 	}
 
-	/*
+	/**
 	 * @return the number of data bytes in a packet
 	 */
 	public int getNumberOfDataBytes() {
 		return ndb;
 	}
 
-	/*
+	/**
 	 * Assemble a burst of packets from the data passed If the number of bytes is
 	 * not a multiple of the packet size, the last packet will have less data bytes.
-	 * 
-	 * @param data - data bytes
-	 * 
+	 * @param data data bytes
 	 * @return assembled packets
 	 */
 	public byte[] pack(byte data[]) {
 		return pack(data, ndb, ncb, p, k);
 	}
 
-	/*
+	/**
 	 * Assemble a burst of packets from the data passed
-	 * 
-	 * @param data - data bytes
-	 * 
-	 * @param nused - package only the first these many bytes
+	 * @param data data bytes
+	 * @param nused package only the first these many bytes
+	 * @return assembled packets
 	 */
 	public byte[] pack(byte data[], int nused) {
 		byte[] dataSubset = new byte[nused];
@@ -68,20 +61,14 @@ public class Hash {
 		return pack(dataSubset, ndb, ncb, p, k);
 	}
 
-	/*
+	/**
 	 * Assemble a burst of packets from the data passed This function allows to pack
 	 * a burst of data without using an actual Hash instance.
-	 * 
-	 * @param data - data bytes
-	 * 
-	 * @param ndatabytes - number of data bytes
-	 * 
-	 * @param ncheckbytes - number of checksum bytes
-	 * 
-	 * @param pattern - hash function pattern
-	 * 
-	 * @param k - hash function k
-	 * 
+	 * @param data data bytes
+	 * @param ndatabytes number of data bytes
+	 * @param ncheckbytes number of checksum bytes
+	 * @param pattern hash function pattern
+	 * @param k hash function k
 	 * @return assembled packets
 	 */
 	public static byte[] pack(byte data[], int ndatabytes, int ncheckbytes, byte pattern, int k) {
@@ -117,35 +104,25 @@ public class Hash {
 		return packet;
 	}
 
-	/*
+	/**
 	 * Disassemble a burst of packets
-	 * 
-	 * @param packets - packet bytes
-	 * 
+	 * @param packets packet bytes
 	 * @return data bytes
-	 * 
 	 * @throws Exception if the checksums are incorrect
 	 */
 	public byte[] unpack(byte packets[]) throws Exception {
 		return unpack(packets, ndb, ncb, p, k);
 	}
 
-	/*
+	/**
 	 * Disassemble a burst of packets
 	 * This function allows to disassemble a burst of packets without actually using a Hash instance.
-	 * 
-	 * @param packets - packet bytes
-	 * 
-	 * @param ndatabytes - number of data bytes
-	 * 
-	 * @param ncheckbytes - number of checksum bytes
-	 * 
-	 * @param pattern - hash function pattern
-	 * 
-	 * @param k - hash function k
-	 * 
+	 * @param packets packet bytes
+	 * @param ndatabytes number of data bytes
+	 * @param ncheckbytes number of checksum bytes
+	 * @param pattern hash function pattern
+	 * @param k hash function k
 	 * @return data stored in the packets
-	 * 
 	 * @throws Exception if the checksum bytes are incorrect
 	 * */
 	public static byte[] unpack(byte packets[], int ndatabytes, int ncheckbytes, byte pattern, int k) throws Exception {
@@ -170,11 +147,9 @@ public class Hash {
 		return data;
 	}
 
-	/*
+	/**
 	 * Assemble and then disassemble some data. Only provided as a test method.
-	 * 
-	 * @param args - explained when run
-	 * 
+	 * @param args explained when run
 	 * @throws Exception gathered while running the program
 	 * */
 	public static void main(String[] args) throws Exception {
